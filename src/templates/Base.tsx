9b27ff41883fad6d70efc5defa92d4cd47bb281b
JasonD100 <dylangove@gmail.com>
2022-07-12 08:53:56 +0000

    embeds?

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
@@ -5,6 +7,24 @@ import { VerticalFeatures } from './VerticalFeatures';
 const Base = () => (
   <div className="antialiased text-retro-black">
     {/* <Meta title={AppConfig.title} description={AppConfig.description} /> */}
+    <Head>
+      <meta property="og:type" content="website" />
+    </Head>
+    <Head>
+      <meta property="og:url" content="https://www.walmartworkersunion.com/" />
+    </Head>
+    <Head>
+      <meta property="og:title" content="Union Benefits | Walmart" />
+    </Head>
+    <Head>
+      <meta
+        property="og:description"
+        content="Unionization Benefits for the larget company in the food industry!"
+      />
+    </Head>
+    <Head>
+      <meta property="og:image" content="/assets/images/wagegap.png" />
+    </Head>
     <NavbarTwoColumns />
     <Hero />
     <VerticalFeatures />
